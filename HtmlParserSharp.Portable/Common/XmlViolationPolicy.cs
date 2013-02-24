bac9e948d191183d8eef70897787f24d052e80ed
John Lam <jflam@microsoft.com>
2013-02-24 09:49:08 -0800

    Resharper clean the types in the Common folder

--- HtmlParserSharp.Portable/Common/XmlViolationPolicy.cs
+++ HtmlParserSharp.Portable/Common/XmlViolationPolicy.cs
@@ -21,11 +21,6 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 namespace HtmlParserSharp.Portable.Common
 {
 	/// <summary>
--- HtmlParserSharp.Portable/SimpleHtmlParser.cs
+++ HtmlParserSharp.Portable/SimpleHtmlParser.cs
@@ -24,20 +24,20 @@
 
 using System;
 using System.IO;
-using HtmlParserSharp.Portable.Core;
 using System.Xml.Linq;
+using HtmlParserSharp.Portable.Core;
 
 namespace HtmlParserSharp.Portable
 {
-	/// <summary>
-	/// This is a simple API for the parsing process.
-	/// Part of this is a port of the nu.validator.htmlparser.io.Driver class.
-	/// The parser currently ignores the encoding in the html source and parses everything as UTF-8.
-	/// </summary>
-	public class SimpleHtmlParser
-	{
-		private Tokenizer _tokenizer;
-		private DomTreeBuilder _treeBuilder;
+    /// <summary>
+    ///     This is a simple API for the parsing process.
+    ///     Part of this is a port of the nu.validator.htmlparser.io.Driver class.
+    ///     The parser currently ignores the encoding in the html source and parses everything as UTF-8.
+    /// </summary>
+    public class SimpleHtmlParser
+    {
+        private Tokenizer _tokenizer;
+        private DomTreeBuilder _treeBuilder;
 
         //public XmlDocumentFragment ParseStringFragment(string str, string fragmentContext)
         //{
@@ -45,11 +45,11 @@ namespace HtmlParserSharp.Portable
         //        return ParseFragment(reader, fragmentContext);
         //}
 
-		public XDocument ParseString(string str)
-		{
-			using (var reader = new StringReader(str))
-				return Parse(reader);
-		}
+        public XDocument ParseString(string str)
+        {
+            using (var reader = new StringReader(str))
+                return Parse(reader);
+        }
 
         //public XDocument Parse(string path)
         //{
@@ -57,12 +57,12 @@ namespace HtmlParserSharp.Portable
         //        return Parse(reader);
         //}
 
-		public XDocument Parse(TextReader reader)
-		{
-			Reset();
-			Tokenize(reader, swallowBom: true);
-			return _treeBuilder.Document;
-		}
+        public XDocument Parse(TextReader reader)
+        {
+            Reset();
+            Tokenize(reader, swallowBom: true);
+            return _treeBuilder.Document;
+        }
 
         //public XmlDocumentFragment ParseFragment(TextReader reader, string fragmentContext)
         //{
@@ -72,92 +72,92 @@ namespace HtmlParserSharp.Portable
         //    return treeBuilder.getDocumentFragment();
         //}
 
-		private void Reset()
-		{
-			_treeBuilder = new DomTreeBuilder();
-			_tokenizer = new Tokenizer(_treeBuilder, false);
-			_treeBuilder.IsIgnoringComments = false;
+        private void Reset()
+        {
+            _treeBuilder = new DomTreeBuilder();
+            _tokenizer = new Tokenizer(_treeBuilder, false);
+            _treeBuilder.IsIgnoringComments = false;
 
-			// optionally: report errors and more
+            // optionally: report errors and more
 
-			//treeBuilder.ErrorEvent +=
-			//    (sender, a) =>
-			//    {
-			//        ILocator loc = tokenizer as ILocator;
-			//        Console.WriteLine("{0}: {1} (Line: {2})", a.IsWarning ? "Warning" : "Error", a.Message, loc.LineNumber);
-			//    };
-			//treeBuilder.DocumentModeDetected += (sender, a) => Console.WriteLine("Document mode: " + a.Mode.ToString());
-			//tokenizer.EncodingDeclared += (sender, a) => Console.WriteLine("Encoding: " + a.Encoding + " (currently ignored)");
-		}
+            //treeBuilder.ErrorEvent +=
+            //    (sender, a) =>
+            //    {
+            //        ILocator loc = tokenizer as ILocator;
+            //        Console.WriteLine("{0}: {1} (Line: {2})", a.IsWarning ? "Warning" : "Error", a.Message, loc.LineNumber);
+            //    };
+            //treeBuilder.DocumentModeDetected += (sender, a) => Console.WriteLine("Document mode: " + a.Mode.ToString());
+            //tokenizer.EncodingDeclared += (sender, a) => Console.WriteLine("Encoding: " + a.Encoding + " (currently ignored)");
+        }
 
-		private void Tokenize(TextReader reader, bool swallowBom)
-		{
-			if (reader == null)
-			{
-			    throw new ArgumentNullException("reader");
-			}
+        private void Tokenize(TextReader reader, bool swallowBom)
+        {
+            if (reader == null)
+            {
+                throw new ArgumentNullException("reader");
+            }
 
-			_tokenizer.Start();
+            _tokenizer.Start();
 
-			try
-			{
-				var buffer = new char[2048];
-				var bufr = new UTF16Buffer(buffer, 0, 0);
-				var lastWasCR = false;
-				int len;
-				if ((len = reader.Read(buffer, 0, buffer.Length)) != 0)
-				{
-					var streamOffset = 0;
-					var offset = 0;
-					var length = len;
-					if (swallowBom)
-					{
-						if (buffer[0] == '\uFEFF')
-						{
-							streamOffset = -1;
-							offset = 1;
-							length--;
-						}
-					}
-					if (length > 0)
-					{
-						_tokenizer.SetTransitionBaseOffset(streamOffset);
-						bufr.Start = offset;
-						bufr.End = offset + length;
-						while (bufr.HasMore)
-						{
-							bufr.Adjust(lastWasCR);
-							lastWasCR = false;
-							if (bufr.HasMore)
-							{
-								lastWasCR = _tokenizer.TokenizeBuffer(bufr);
-							}
-						}
-					}
-					streamOffset = length;
-					while ((len = reader.Read(buffer, 0, buffer.Length)) != 0)
-					{
-						_tokenizer.SetTransitionBaseOffset(streamOffset);
-						bufr.Start = 0;
-						bufr.End = len;
-						while (bufr.HasMore)
-						{
-							bufr.Adjust(lastWasCR);
-							lastWasCR = false;
-							if (bufr.HasMore)
-							{
-								lastWasCR = _tokenizer.TokenizeBuffer(bufr);
-							}
-						}
-						streamOffset += len;
-					}
-				}
-				_tokenizer.Eof();
-			}
-			finally
-			{
-				_tokenizer.End();
-			}
-		}
-	}
+            try
+            {
+                var buffer = new char[2048];
+                var bufr = new UTF16Buffer(buffer, 0, 0);
+                bool lastWasCR = false;
+                int len;
+                if ((len = reader.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    int streamOffset = 0;
+                    int offset = 0;
+                    int length = len;
+                    if (swallowBom)
+                    {
+                        if (buffer[0] == '\uFEFF')
+                        {
+                            streamOffset = -1;
+                            offset = 1;
+                            length--;
+                        }
+                    }
+                    if (length > 0)
+                    {
+                        _tokenizer.SetTransitionBaseOffset(streamOffset);
+                        bufr.Start = offset;
+                        bufr.End = offset + length;
+                        while (bufr.HasMore)
+                        {
+                            bufr.Adjust(lastWasCR);
+                            lastWasCR = false;
+                            if (bufr.HasMore)
+                            {
+                                lastWasCR = _tokenizer.TokenizeBuffer(bufr);
+                            }
+                        }
+                    }
+                    streamOffset = length;
+                    while ((len = reader.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        _tokenizer.SetTransitionBaseOffset(streamOffset);
+                        bufr.Start = 0;
+                        bufr.End = len;
+                        while (bufr.HasMore)
+                        {
+                            bufr.Adjust(lastWasCR);
+                            lastWasCR = false;
+                            if (bufr.HasMore)
+                            {
+                                lastWasCR = _tokenizer.TokenizeBuffer(bufr);
+                            }
+                        }
+                        streamOffset += len;
+                    }
+                }
+                _tokenizer.Eof();
+            }
+            finally
+            {
+                _tokenizer.End();
+            }
+        }
+    }
 }
