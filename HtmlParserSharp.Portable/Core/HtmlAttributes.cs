3cf38e42f7764fbf07b753c6febfab078ace49e1
John Lam <jflam@microsoft.com>
2013-02-24 13:52:12 -0800

    Rename member fields to use _ naming convention

--- HtmlParserSharp.Portable/Core/HtmlAttributes.cs
+++ HtmlParserSharp.Portable/Core/HtmlAttributes.cs
@@ -44,46 +44,46 @@ namespace HtmlParserSharp.Portable.Core
 
 		public static readonly HtmlAttributes EMPTY_ATTRIBUTES = new HtmlAttributes(AttributeName.HTML);
 
-		private int mode;
+		private int _mode;
 
-		private int length;
+		private int _length;
 
-		private AttributeName[] names;
+		private AttributeName[] _names;
 
-		private string[] values;
+		private string[] _values;
 
 		// [NOCPP[
 
-		private string idValue;
+		private string _idValue;
 
-		private int xmlnsLength;
+		private int _xmlnsLength;
 
-		private AttributeName[] xmlnsNames;
+		private AttributeName[] _xmlnsNames;
 
-		private string[] xmlnsValues;
+		private string[] _xmlnsValues;
 
 		// ]NOCPP]
 
 		public HtmlAttributes(int mode)
 		{
-			this.mode = mode;
-			this.length = 0;
+			this._mode = mode;
+			this._length = 0;
 			/*
 			 * The length of 5 covers covers 98.3% of elements
 			 * according to Hixie
 			 */
-			this.names = new AttributeName[5];
-			this.values = new string[5];
+			this._names = new AttributeName[5];
+			this._values = new string[5];
 
 			// [NOCPP[
 
-			this.idValue = null;
+			this._idValue = null;
 
-			this.xmlnsLength = 0;
+			this._xmlnsLength = 0;
 
-			this.xmlnsNames = HtmlAttributes.EMPTY_ATTRIBUTENAMES;
+			this._xmlnsNames = HtmlAttributes.EMPTY_ATTRIBUTENAMES;
 
-			this.xmlnsValues = HtmlAttributes.EMPTY_stringS;
+			this._xmlnsValues = HtmlAttributes.EMPTY_stringS;
 
 			// ]NOCPP]
 		}
@@ -107,9 +107,9 @@ namespace HtmlParserSharp.Portable.Core
 		/// </summary>
 		public int GetIndex(AttributeName name)
 		{
-			for (int i = 0; i < length; i++)
+			for (int i = 0; i < _length; i++)
 			{
-				if (names[i] == name)
+				if (_names[i] == name)
 				{
 					return i;
 				}
@@ -121,9 +121,9 @@ namespace HtmlParserSharp.Portable.Core
 
 		public int GetIndex(string qName)
 		{
-			for (int i = 0; i < length; i++)
+			for (int i = 0; i < _length; i++)
 			{
-				if (names[i].GetQName(mode) == qName)
+				if (_names[i].GetQName(_mode) == qName)
 				{
 					return i;
 				}
@@ -133,10 +133,10 @@ namespace HtmlParserSharp.Portable.Core
 
 		public int GetIndex(string uri, string localName)
 		{
-			for (int i = 0; i < length; i++)
+			for (int i = 0; i < _length; i++)
 			{
-				if (names[i].GetLocal(mode) == localName
-						&& names[i].GetUri(mode) == uri)
+				if (_names[i].GetLocal(_mode) == localName
+						&& _names[i].GetUri(_mode) == uri)
 				{
 					return i;
 				}
@@ -202,16 +202,16 @@ namespace HtmlParserSharp.Portable.Core
 		{
 			get
 			{
-				return length;
+				return _length;
 			}
 		}
 
 		[Local]
 		public string GetLocalName(int index)
 		{
-			if (index < length && index >= 0)
+			if (index < _length && index >= 0)
 			{
-				return names[index].GetLocal(mode);
+				return _names[index].GetLocal(_mode);
 			}
 			else
 			{
@@ -223,9 +223,9 @@ namespace HtmlParserSharp.Portable.Core
 
 		public string GetQName(int index)
 		{
-			if (index < length && index >= 0)
+			if (index < _length && index >= 0)
 			{
-				return names[index].GetQName(mode);
+				return _names[index].GetQName(_mode);
 			}
 			else
 			{
@@ -235,9 +235,9 @@ namespace HtmlParserSharp.Portable.Core
 
 		public string GetType(int index)
 		{
-			if (index < length && index >= 0)
+			if (index < _length && index >= 0)
 			{
-				return (names[index] == AttributeName.ID) ? "ID" : "CDATA";
+				return (_names[index] == AttributeName.ID) ? "ID" : "CDATA";
 			}
 			else
 			{
@@ -249,9 +249,9 @@ namespace HtmlParserSharp.Portable.Core
 
 		public AttributeName GetAttributeName(int index)
 		{
-			if (index < length && index >= 0)
+			if (index < _length && index >= 0)
 			{
-				return names[index];
+				return _names[index];
 			}
 			else
 			{
@@ -262,9 +262,9 @@ namespace HtmlParserSharp.Portable.Core
 		[NsUri]
 		public string GetURI(int index)
 		{
-			if (index < length && index >= 0)
+			if (index < _length && index >= 0)
 			{
-				return names[index].GetUri(mode);
+				return _names[index].GetUri(_mode);
 			}
 			else
 			{
@@ -275,9 +275,9 @@ namespace HtmlParserSharp.Portable.Core
 		[Prefix]
 		public string GetPrefix(int index)
 		{
-			if (index < length && index >= 0)
+			if (index < _length && index >= 0)
 			{
-				return names[index].GetPrefix(mode);
+				return _names[index].GetPrefix(_mode);
 			}
 			else
 			{
@@ -287,9 +287,9 @@ namespace HtmlParserSharp.Portable.Core
 
 		public string GetValue(int index)
 		{
-			if (index < length && index >= 0)
+			if (index < _length && index >= 0)
 			{
-				return values[index];
+				return _values[index];
 			}
 			else
 			{
@@ -319,7 +319,7 @@ namespace HtmlParserSharp.Portable.Core
 		{
 			get
 			{
-				return idValue;
+				return _idValue;
 			}
 		}
 
@@ -327,16 +327,16 @@ namespace HtmlParserSharp.Portable.Core
 		{
 			get
 			{
-				return xmlnsLength;
+				return _xmlnsLength;
 			}
 		}
 
 		[Local]
 		public string GetXmlnsLocalName(int index)
 		{
-			if (index < xmlnsLength && index >= 0)
+			if (index < _xmlnsLength && index >= 0)
 			{
-				return xmlnsNames[index].GetLocal(mode);
+				return _xmlnsNames[index].GetLocal(_mode);
 			}
 			else
 			{
@@ -347,9 +347,9 @@ namespace HtmlParserSharp.Portable.Core
 		[NsUri]
 		public string GetXmlnsURI(int index)
 		{
-			if (index < xmlnsLength && index >= 0)
+			if (index < _xmlnsLength && index >= 0)
 			{
-				return xmlnsNames[index].GetUri(mode);
+				return _xmlnsNames[index].GetUri(_mode);
 			}
 			else
 			{
@@ -359,9 +359,9 @@ namespace HtmlParserSharp.Portable.Core
 
 		public string GetXmlnsValue(int index)
 		{
-			if (index < xmlnsLength && index >= 0)
+			if (index < _xmlnsLength && index >= 0)
 			{
-				return xmlnsValues[index];
+				return _xmlnsValues[index];
 			}
 			else
 			{
@@ -371,9 +371,9 @@ namespace HtmlParserSharp.Portable.Core
 
 		public int GetXmlnsIndex(AttributeName name)
 		{
-			for (int i = 0; i < xmlnsLength; i++)
+			for (int i = 0; i < _xmlnsLength; i++)
 			{
-				if (xmlnsNames[i] == name)
+				if (_xmlnsNames[i] == name)
 				{
 					return i;
 				}
@@ -396,9 +396,9 @@ namespace HtmlParserSharp.Portable.Core
 
 		public AttributeName GetXmlnsAttributeName(int index)
 		{
-			if (index < xmlnsLength && index >= 0)
+			if (index < _xmlnsLength && index >= 0)
 			{
-				return xmlnsNames[index];
+				return _xmlnsNames[index];
 			}
 			else
 			{
@@ -417,24 +417,24 @@ namespace HtmlParserSharp.Portable.Core
 			// [NOCPP[
 			if (name == AttributeName.ID)
 			{
-				idValue = value;
+				_idValue = value;
 			}
 
 			if (name.IsXmlns)
 			{
-				if (xmlnsNames.Length == xmlnsLength)
+				if (_xmlnsNames.Length == _xmlnsLength)
 				{
-					int newLen = xmlnsLength == 0 ? 2 : xmlnsLength << 1;
+					int newLen = _xmlnsLength == 0 ? 2 : _xmlnsLength << 1;
 					AttributeName[] newNames = new AttributeName[newLen];
-					Array.Copy(xmlnsNames, newNames, xmlnsNames.Length);
-					xmlnsNames = newNames;
+					Array.Copy(_xmlnsNames, newNames, _xmlnsNames.Length);
+					_xmlnsNames = newNames;
 					string[] newValues = new string[newLen];
-					Array.Copy(xmlnsValues, newValues, xmlnsValues.Length);
-					xmlnsValues = newValues;
+					Array.Copy(_xmlnsValues, newValues, _xmlnsValues.Length);
+					_xmlnsValues = newValues;
 				}
-				xmlnsNames[xmlnsLength] = name;
-				xmlnsValues[xmlnsLength] = value;
-				xmlnsLength++;
+				_xmlnsNames[_xmlnsLength] = name;
+				_xmlnsValues[_xmlnsLength] = value;
+				_xmlnsLength++;
 				switch (xmlnsPolicy)
 				{
 					case XmlViolationPolicy.Fatal:
@@ -449,40 +449,40 @@ namespace HtmlParserSharp.Portable.Core
 
 			// ]NOCPP]
 
-			if (names.Length == length)
+			if (_names.Length == _length)
 			{
-				int newLen = length << 1; // The first growth covers virtually
+				int newLen = _length << 1; // The first growth covers virtually
 				// 100% of elements according to
 				// Hixie
 				AttributeName[] newNames = new AttributeName[newLen];
-				Array.Copy(names, newNames, names.Length);
-				names = newNames;
+				Array.Copy(_names, newNames, _names.Length);
+				_names = newNames;
 				string[] newValues = new string[newLen];
-				Array.Copy(values, newValues, values.Length);
-				values = newValues;
+				Array.Copy(_values, newValues, _values.Length);
+				_values = newValues;
 			}
-			names[length] = name;
-			values[length] = value;
-			length++;
+			_names[_length] = name;
+			_values[_length] = value;
+			_length++;
 		}
 
 		internal void Clear(int m)
 		{
-			for (int i = 0; i < length; i++)
+			for (int i = 0; i < _length; i++)
 			{
-				names[i] = null;
-				values[i] = null;
+				_names[i] = null;
+				_values[i] = null;
 			}
-			length = 0;
-			mode = m;
+			_length = 0;
+			_mode = m;
 			// [NOCPP[
-			idValue = null;
-			for (int i = 0; i < xmlnsLength; i++)
+			_idValue = null;
+			for (int i = 0; i < _xmlnsLength; i++)
 			{
-				xmlnsNames[i] = null;
-				xmlnsValues[i] = null;
+				_xmlnsNames[i] = null;
+				_xmlnsValues[i] = null;
 			}
-			xmlnsLength = 0;
+			_xmlnsLength = 0;
 			// ]NOCPP]
 		}
 
@@ -492,27 +492,27 @@ namespace HtmlParserSharp.Portable.Core
 		/// </summary>
 		internal void ClearWithoutReleasingContents()
 		{
-			for (int i = 0; i < length; i++)
+			for (int i = 0; i < _length; i++)
 			{
-				names[i] = null;
-				values[i] = null;
+				_names[i] = null;
+				_values[i] = null;
 			}
-			length = 0;
+			_length = 0;
 		}
 
 		public bool Contains(AttributeName name)
 		{
-			for (int i = 0; i < length; i++)
+			for (int i = 0; i < _length; i++)
 			{
-				if (name.EqualsAnother(names[i]))
+				if (name.EqualsAnother(_names[i]))
 				{
 					return true;
 				}
 			}
 			// [NOCPP[
-			for (int i = 0; i < xmlnsLength; i++)
+			for (int i = 0; i < _xmlnsLength; i++)
 			{
-				if (name.EqualsAnother(xmlnsNames[i]))
+				if (name.EqualsAnother(_xmlnsNames[i]))
 				{
 					return true;
 				}
@@ -523,31 +523,31 @@ namespace HtmlParserSharp.Portable.Core
 
 		public void AdjustForMath()
 		{
-			mode = AttributeName.MATHML;
+			_mode = AttributeName.MATHML;
 		}
 
 		public void AdjustForSvg()
 		{
-			mode = AttributeName.SVG;
+			_mode = AttributeName.SVG;
 		}
 
 		public HtmlAttributes CloneAttributes()
 		{
-			Debug.Assert((length == 0 && xmlnsLength == 0) || mode == 0 || mode == 3);
+			Debug.Assert((_length == 0 && _xmlnsLength == 0) || _mode == 0 || _mode == 3);
 			HtmlAttributes clone = new HtmlAttributes(0);
-			for (int i = 0; i < length; i++)
+			for (int i = 0; i < _length; i++)
 			{
-				clone.AddAttribute(names[i].CloneAttributeName(), values[i]
+				clone.AddAttribute(_names[i].CloneAttributeName(), _values[i]
 					// [NOCPP[
 					   , XmlViolationPolicy.Allow
 					// ]NOCPP]
 				);
 			}
 			// [NOCPP[
-			for (int i = 0; i < xmlnsLength; i++)
+			for (int i = 0; i < _xmlnsLength; i++)
 			{
-				clone.AddAttribute(xmlnsNames[i],
-						xmlnsValues[i], XmlViolationPolicy.Allow);
+				clone.AddAttribute(_xmlnsNames[i],
+						_xmlnsValues[i], XmlViolationPolicy.Allow);
 			}
 			// ]NOCPP]
 			return clone; // XXX!!!
@@ -555,26 +555,26 @@ namespace HtmlParserSharp.Portable.Core
 
 		public bool Equals(HtmlAttributes other)
 		{
-			Debug.Assert(mode == 0 || mode == 3, "Trying to compare attributes in foreign content.");
+			Debug.Assert(_mode == 0 || _mode == 3, "Trying to compare attributes in foreign content.");
 			int otherLength = other.Length;
-			if (length != otherLength)
+			if (_length != otherLength)
 			{
 				return false;
 			}
-			for (int i = 0; i < length; i++)
+			for (int i = 0; i < _length; i++)
 			{
 				// Work around the limitations of C++
 				bool found = false;
 				// The comparing just the local names is OK, since these attribute
 				// holders are both supposed to belong to HTML formatting elements
 				/*[Local]*/
-				string ownLocal = names[i].GetLocal(AttributeName.HTML);
+				string ownLocal = _names[i].GetLocal(AttributeName.HTML);
 				for (int j = 0; j < otherLength; j++)
 				{
-					if (ownLocal == other.names[j].GetLocal(AttributeName.HTML))
+					if (ownLocal == other._names[j].GetLocal(AttributeName.HTML))
 					{
 						found = true;
-						if (values[i] != other.values[j])
+						if (_values[i] != other._values[j])
 						{
 							return false;
 						}
@@ -592,16 +592,16 @@ namespace HtmlParserSharp.Portable.Core
 
 		internal void ProcessNonNcNames<T>(TreeBuilder<T> treeBuilder, XmlViolationPolicy namePolicy) where T : class
 		{
-			for (int i = 0; i < length; i++)
+			for (int i = 0; i < _length; i++)
 			{
-				AttributeName attName = names[i];
-				if (!attName.IsNcName(mode))
+				AttributeName attName = _names[i];
+				if (!attName.IsNcName(_mode))
 				{
-					string name = attName.GetLocal(mode);
+					string name = attName.GetLocal(_mode);
 					switch (namePolicy)
 					{
 						case XmlViolationPolicy.AlterInfoset:
-							names[i] = AttributeName.Create(NCName.EscapeName(name));
+							_names[i] = AttributeName.Create(NCName.EscapeName(name));
 							goto case XmlViolationPolicy.Allow; // fall through
 						case XmlViolationPolicy.Allow:
 							if (attName != AttributeName.XML_LANG)
