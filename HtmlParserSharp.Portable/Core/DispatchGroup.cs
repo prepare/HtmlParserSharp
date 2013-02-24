7517225fe420b47a83695db46f13b7fe18be9c68
John Lam <jflam@microsoft.com>
2013-02-24 13:48:09 -0800

    Resharper mechanical code formatting changes

--- HtmlParserSharp.Portable/Core/DispatchGroup.cs
+++ HtmlParserSharp.Portable/Core/DispatchGroup.cs
@@ -26,136 +26,137 @@
 
 namespace HtmlParserSharp.Portable.Core
 {
-	public enum DispatchGroup
-	{
-		OTHER = 0,
+    public enum DispatchGroup
+    {
+        OTHER = 0,
 
-		A = 1,
+        A = 1,
 
-		BASE = 2,
+        BASE = 2,
 
-		BODY = 3,
+        BODY = 3,
 
-		BR = 4,
+        BR = 4,
 
-		BUTTON = 5,
+        BUTTON = 5,
 
-		CAPTION = 6,
+        CAPTION = 6,
 
-		COL = 7,
+        COL = 7,
 
-		COLGROUP = 8,
+        COLGROUP = 8,
 
-		FORM = 9,
+        FORM = 9,
 
-		FRAME = 10,
+        FRAME = 10,
 
-		FRAMESET = 11,
+        FRAMESET = 11,
 
-		IMAGE = 12,
+        IMAGE = 12,
 
-		INPUT = 13,
+        INPUT = 13,
 
-		ISINDEX = 14,
+        ISINDEX = 14,
 
-		LI = 15,
+        LI = 15,
 
-		LINK_OR_BASEFONT_OR_BGSOUND = 16,
+        LINK_OR_BASEFONT_OR_BGSOUND = 16,
 
-		MATH = 17,
+        MATH = 17,
 
-		META = 18,
+        META = 18,
 
-		SVG = 19,
+        SVG = 19,
 
-		HEAD = 20,
+        HEAD = 20,
 
-		HR = 22,
+        HR = 22,
 
-		HTML = 23,
+        HTML = 23,
 
-		NOBR = 24,
+        NOBR = 24,
 
-		NOFRAMES = 25,
+        NOFRAMES = 25,
 
-		NOSCRIPT = 26,
+        NOSCRIPT = 26,
 
-		OPTGROUP = 27,
+        OPTGROUP = 27,
 
-		OPTION = 28,
+        OPTION = 28,
 
-		P = 29,
+        P = 29,
 
-		PLAINTEXT = 30,
+        PLAINTEXT = 30,
 
-		SCRIPT = 31,
+        SCRIPT = 31,
 
-		SELECT = 32,
+        SELECT = 32,
 
-		STYLE = 33,
+        STYLE = 33,
 
-		TABLE = 34,
+        TABLE = 34,
 
-		TEXTAREA = 35,
+        TEXTAREA = 35,
 
-		TITLE = 36,
+        TITLE = 36,
 
-		TR = 37,
+        TR = 37,
 
-		XMP = 38,
+        XMP = 38,
 
-		TBODY_OR_THEAD_OR_TFOOT = 39,
+        TBODY_OR_THEAD_OR_TFOOT = 39,
 
-		TD_OR_TH = 40,
+        TD_OR_TH = 40,
 
-		DD_OR_DT = 41,
+        DD_OR_DT = 41,
 
-		H1_OR_H2_OR_H3_OR_H4_OR_H5_OR_H6 = 42,
+        H1_OR_H2_OR_H3_OR_H4_OR_H5_OR_H6 = 42,
 
-		MARQUEE_OR_APPLET = 43,
+        MARQUEE_OR_APPLET = 43,
 
-		PRE_OR_LISTING = 44,
+        PRE_OR_LISTING = 44,
 
-		B_OR_BIG_OR_CODE_OR_EM_OR_I_OR_S_OR_SMALL_OR_STRIKE_OR_STRONG_OR_TT_OR_U = 45,
+        B_OR_BIG_OR_CODE_OR_EM_OR_I_OR_S_OR_SMALL_OR_STRIKE_OR_STRONG_OR_TT_OR_U = 45,
 
-		UL_OR_OL_OR_DL = 46,
+        UL_OR_OL_OR_DL = 46,
 
-		IFRAME = 47,
+        IFRAME = 47,
 
-		EMBED_OR_IMG = 48,
+        EMBED_OR_IMG = 48,
 
-		AREA_OR_WBR = 49,
+        AREA_OR_WBR = 49,
 
-		DIV_OR_BLOCKQUOTE_OR_CENTER_OR_MENU = 50,
+        DIV_OR_BLOCKQUOTE_OR_CENTER_OR_MENU = 50,
 
-		ADDRESS_OR_ARTICLE_OR_ASIDE_OR_DETAILS_OR_DIR_OR_FIGCAPTION_OR_FIGURE_OR_FOOTER_OR_HEADER_OR_HGROUP_OR_NAV_OR_SECTION_OR_SUMMARY = 51,
+        ADDRESS_OR_ARTICLE_OR_ASIDE_OR_DETAILS_OR_DIR_OR_FIGCAPTION_OR_FIGURE_OR_FOOTER_OR_HEADER_OR_HGROUP_OR_NAV_OR_SECTION_OR_SUMMARY
+            = 51,
 
-		RUBY_OR_SPAN_OR_SUB_OR_SUP_OR_VAR = 52,
+        RUBY_OR_SPAN_OR_SUB_OR_SUP_OR_VAR = 52,
 
-		RT_OR_RP = 53,
+        RT_OR_RP = 53,
 
-		COMMAND = 54,
+        COMMAND = 54,
 
-		PARAM_OR_SOURCE_OR_TRACK = 55,
+        PARAM_OR_SOURCE_OR_TRACK = 55,
 
-		MGLYPH_OR_MALIGNMARK = 56,
+        MGLYPH_OR_MALIGNMARK = 56,
 
-		MI_MO_MN_MS_MTEXT = 57,
+        MI_MO_MN_MS_MTEXT = 57,
 
-		ANNOTATION_XML = 58,
+        ANNOTATION_XML = 58,
 
-		FOREIGNOBJECT_OR_DESC = 59,
+        FOREIGNOBJECT_OR_DESC = 59,
 
-		NOEMBED = 60,
+        NOEMBED = 60,
 
-		FIELDSET = 61,
+        FIELDSET = 61,
 
-		OUTPUT_OR_LABEL = 62,
+        OUTPUT_OR_LABEL = 62,
 
-		OBJECT = 63,
+        OBJECT = 63,
 
-		FONT = 64,
+        FONT = 64,
 
-		KEYGEN = 65
-	}
+        KEYGEN = 65
+    }
 }
