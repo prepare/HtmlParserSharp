9d00a7b4e1015a36f870a3d74c0de5b3347622da
John Lam <jflam@microsoft.com>
2013-02-27 20:54:48 -0800

    Refactored charset encoding unit tests
    Found a bug in the test-yahoo-jp.dat file and fixed it.
    There are definitely bugs in attribute value parsing today on corner
    cases.

---
+++ HtmlParserSharp.Tests/TestData/CharsetEncodingTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using HtmlParserSharp.Portable;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlParserSharp.Tests.TestData
+{
+    [TestClass]
+    public class CharsetEncodingTests
+    {
+        class EncodingTestCase
+        {
+            public string Html { get; set; }
+            public string Encoding { get; set; }
+        }
+
+        enum TestDataParserStates
+        {
+            ReadingHtml,
+            ReaadingEncoding,
+            ReadingWhitespace
+        }
+
+        private List<EncodingTestCase> ParseTestData(string path)
+        {
+            var result = new List<EncodingTestCase>();
+            var parserState = TestDataParserStates.ReadingWhitespace;
+
+            using (var reader = new StreamReader(path))
+            {
+                var testCase = new EncodingTestCase();
+                var htmlBuilder = new StringBuilder();
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (line.Contains("#data"))
+                    {
+                        parserState = TestDataParserStates.ReadingHtml;
+                        htmlBuilder.Clear();
+                    }
+                    else if (line.Contains("#encoding"))
+                    {
+                        parserState = TestDataParserStates.ReaadingEncoding;
+                        testCase.Html = htmlBuilder.ToString();
+                    }
+                    else
+                    {
+                        if (parserState == TestDataParserStates.ReaadingEncoding)
+                        {
+                            testCase.Encoding = line;
+                            parserState = TestDataParserStates.ReadingWhitespace;
+                            result.Add(testCase);
+                            testCase = new EncodingTestCase();
+                        } 
+                        else if (parserState == TestDataParserStates.ReadingHtml)
+                        {
+                            htmlBuilder.Append(line + "\n");
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        public async Task RunTestFile(string path)
+        {
+            List<EncodingTestCase> testCases = ParseTestData(path);
+            var parser = new SimpleHtmlParser();
+            int counter = 0;
+            List<int> errorOffsets = new List<int>();
+            foreach (var testCase in testCases)
+            {
+                var doc = await parser.ParseString(testCase.Html);
+                if (!string.Equals(testCase.Encoding, parser.DocumentEncoding, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    errorOffsets.Add(counter);
+                }
+                counter++;
+            }
+            Assert.AreEqual(0, errorOffsets.Count, "Number of errors is {0}/{1}", errorOffsets.Count, testCases.Count);
+        }
+
+        [TestMethod]
+        [DeploymentItem("TestData")]
+        public async Task EncodingTests1()
+        {
+            await RunTestFile("Encodings\\tests1.dat");
+        }
+
+        [TestMethod]
+        [DeploymentItem("TestData")]
+        public async Task EncodingTests2()
+        {
+            await RunTestFile("Encodings\\tests2.dat");
+        }
+
+        [TestMethod]
+        [DeploymentItem("TestData")]
+        public async Task EncodingTests3()
+        {
+            await RunTestFile("Encodings\\test-yahoo-jp.dat");
+        }
+    }
+}
--- HtmlParserSharp.Tests/WholeDocumentTests.cs
+++ HtmlParserSharp.Tests/WholeDocumentTests.cs
@@ -1,8 +1,6 @@
 ﻿using HtmlParserSharp.Portable;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -25,81 +23,6 @@ namespace HtmlParserSharp.Tests
                 // be somewhere on the document, but not sure where on the XDocument it belongs
                 Assert.AreEqual("ISO-8859-1", parser.DocumentEncoding);
             }
-        }
-
-        class EncodingTestCase
-        {
-            public string Html { get; set; }
-            public string Encoding { get; set; }
-        }
-
-        enum TestDataParserStates
-        {
-            ReadingHtml,
-            ReaadingEncoding,
-            ReadingWhitespace
-        }
-
-        private List<EncodingTestCase> ParseTestData(string path)
-        {
-            var result = new List<EncodingTestCase>();
-            var parserState = TestDataParserStates.ReadingWhitespace;
-
-            using (var reader = new StreamReader(path))
-            {
-                var testCase = new EncodingTestCase();
-                var htmlBuilder = new StringBuilder();
-
-                while (!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-                    if (line.Contains("#data"))
-                    {
-                        parserState = TestDataParserStates.ReadingHtml;
-                        htmlBuilder.Clear();
-                    }
-                    else if (line.Contains("#encoding"))
-                    {
-                        parserState = TestDataParserStates.ReaadingEncoding;
-                        testCase.Html = htmlBuilder.ToString();
-                    }
-                    else
-                    {
-                        if (parserState == TestDataParserStates.ReaadingEncoding)
-                        {
-                            testCase.Encoding = line;
-                            parserState = TestDataParserStates.ReadingWhitespace;
-                            result.Add(testCase);
-                            testCase = new EncodingTestCase();
-                        } 
-                        else if (parserState == TestDataParserStates.ReadingHtml)
-                        {
-                            htmlBuilder.Append(line + "\n");
-                        }
-                    }
-                }
-            }
-            return result;
-        }
-
-        [TestMethod]
-        [DeploymentItem("TestData")]
-        public async Task EncodingTests()
-        {
-            List<EncodingTestCase> testCases = ParseTestData("Encodings\\tests1.dat");
-            var parser = new SimpleHtmlParser();
-            int counter = 0;
-            List<int> errorOffsets = new List<int>();
-            foreach (var testCase in testCases)
-            {
-                var doc = await parser.ParseString(testCase.Html);
-                if (testCase.Encoding != parser.DocumentEncoding)
-                {
-                    errorOffsets.Add(counter);
-                }
-                counter++;
-            }
-            Assert.AreEqual(0, errorOffsets.Count, "Number of errors is {0}/{1}", errorOffsets.Count, testCases.Count);
         }
     }
 }
