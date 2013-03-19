2a4c3182e1279eb6adb56c8939bb07d9ae19875f
John Lam <jflam@microsoft.com>
2013-03-18 22:59:48 -0700

    Add some additional XHtml tags

--- HtmlParserSharp.Portable/Common/XHtml.cs
+++ HtmlParserSharp.Portable/Common/XHtml.cs
@@ -6,6 +6,8 @@ namespace HtmlParserSharp.Portable.Common
     {
         public static readonly XNamespace NS = "http://www.w3.org/1999/xhtml";
         public static readonly XName HTML = NS + "html";
+        public static readonly XName HEAD = NS + "head";
+        public static readonly XName TITLE = NS + "title";
         public static readonly XName BODY = NS + "body";
         public static readonly XName CENTER = NS + "center";
         public static readonly XName TABLE = NS + "table";
@@ -19,5 +21,10 @@ namespace HtmlParserSharp.Portable.Common
         public static readonly XName FONT = NS + "font";
         public static readonly XName I = NS + "i";
         public static readonly XName P = NS + "p";
+        public static readonly XName UL = NS + "ul";
+        public static readonly XName OL = NS + "ol";
+        public static readonly XName LI = NS + "li";
+        public static readonly XName TIME = NS + "time";
+        public static readonly XName FORM = NS + "form";
     }
 }
